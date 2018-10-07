9afbd9e656687e3d1e78c7e90ea27b9eeec9997e
Artem-Br <bartem.777@gmail.com>
2018-10-07 22:01:46 +0500

    finish homework

--- 02_04.rb
+++ 02_04.rb
@@ -1,9 +1,17 @@
 =begin
 Заполнить хеш гласными буквами, 
 где значением будет являтся порядковый номер буквы в алфавите (a - 1)
+согласные
+b, c, d, f, g, h, j, k, l, m, n, p, q, r, s, t, v, w, x, y, z
+|| "d"|| "f"|| "g"|| "h"|| "j"|| "k"|| "l"|| "m"|| "n"|| "p"|| "q"|| "r"|| "s"|| "t"|| "v"|| "w"|| "x"|| "y"|| "z"
+
 =end
 
-hash = {}
 letter = ('a'..'z').to_a
-namber = (1..28).to_a
+namber = (1..26).to_a
+array = [letter, namber]
 
+hash = Hash[*array.transpose.flatten]
+hash.delete_if{ |key, val| key == "b" and "c"}
+
+puts hash
